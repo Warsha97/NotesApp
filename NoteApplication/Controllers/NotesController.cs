4f1d7caef7da2930b10cc134831ea28b42cb872d
Savini Abayaratne <savvaabayaratne@gmail.com>
2019-07-25 11:09:13 +0530

    Changes done to the controller.

--- NoteApplication/Controllers/NotesController.cs
+++ NoteApplication/Controllers/NotesController.cs
@@ -3,16 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-<<<<<<< HEAD
-using NoteApplication.Models;
 using NoteApplication.BusinessService;
-=======
 using NoteApplication.DataAccess;
-using NoteApplication.BusinessService;
 using NoteApplication.DataAccess.Models;
 
->>>>>>> conflictResolver
+
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -22,85 +17,16 @@ namespace NoteApplication.Controllers
     [ApiController]
     public class NotesController : Controller
     {
-<<<<<<< HEAD
 
-
-
-        // private readonly INotesRepository _notesRepository;
-=======
-        
->>>>>>> conflictResolver
         private readonly INoteService _noteService;
 
 
         public NotesController(INoteService noteService)
         {
             _noteService = noteService;
-<<<<<<< HEAD
+
         }
 
-       // public NotesController(NotesDBContext context)
-        //{
-          //  _context = context;
-
-            //if(_context.Notes.Count() == 0)
-            //{
-              //  _context.Notes.Add(new Notes { Note = "Note1" });
-                //_context.SaveChanges();
-            //}
-        //}
-        // GET: api/<controller>
-        [HttpGet]
-       // [Route("api/[controller]/list")]
-        //public ActionResult GetNotes()
-        public IEnumerable<Notes> GetNotes()
-        {
-            //return await _context.Notes.ToListAsync();
-            // return _context.Notes.ToList();
-            //return _notesRepository.GetNotes();
-            //return View(_notesRepository.GetNotes());
-            return _noteService.GetNotesService();
-        }
-
-        // POST api/<controller>
-
-        // [Route("api/[controller]/new-note")]
-        /* public IActionResult AddNote(Notes note)
-         {
-             if (ModelState.IsValid)
-             {
-                 Notes newNote = _notesRepository.AddNote(note);
-             }
-             return View(note);
-         }  */
-        //[HttpPost]
-        //public int Post(Notes note)
-        //{
-            
-         //   return _noteService.AddNoteService(note);
-        //}
-
-        [HttpPost]
-        public int Post(Notes createdNote)
-        {
-
-        return _noteService.AddNoteService(createdNote);
-
-
-
-    }
-
-
-        // PUT api/<controller>/5
-        [HttpPut("{id}")]
-       // [Route("api/[controller]/changed-note/{id}")]
-        public Notes Update(Notes changedNote)
-        {
-            // _notesRepository.UpdateNote(changedNote);
-            //return changedNote;
-=======
-           
-        }
 
 
         
@@ -126,42 +52,28 @@ namespace NoteApplication.Controllers
        
         public NoteDTO Update(NoteDTO changedNote)
         {
->>>>>>> conflictResolver
+
             _noteService.UpdateNoteService(changedNote);
             return changedNote;
         }
 
-<<<<<<< HEAD
-        // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-      //  [Route("api/[controller]/deleted-note/{id}")]
-        public Notes Delete(int id)
-        {
-            //return _notesRepository.DeleteNote(id);
-=======
+
         [HttpDelete("{id}")]
         
         public Boolean Delete(int id)
         {
->>>>>>> conflictResolver
+
             return _noteService.DeleteNoteService(id);
         }
 
         [HttpGet("{selectedDate}")]
-<<<<<<< HEAD
-        public IEnumerable<Notes> GetNotesByDate(DateTime selectedDate)
-        {
-            //return _notesRepository.GetNotesOfDate(selectedDate);
-            return _noteService.GetNotesByDateService(selectedDate);
-        }
-            
-=======
+
+       
         public IEnumerable<NoteDTO> GetNotesByDate(DateTime selectedDate)
         {
            return _noteService.GetNotesByDateService(selectedDate);
         }
 
->>>>>>> conflictResolver
 
     }
 }
